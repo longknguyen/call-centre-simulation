bda44e6bc62d537f4b5adb5a2540f0586be44676
JamOldacre <bas8pt@virginia.edu>
2025-10-13 00:15:22 -0400

    Initial LCG

--- src/main/java/edu/virginia/apma/probability/LCG.java
+++ src/main/java/edu/virginia/apma/probability/LCG.java
@@ -1,5 +1,7 @@
 package edu.virginia.apma.probability;
 
+import java.math.BigInteger;
+
 /**
  * Linear Congruential Generator (LCG) for generating pseudo-random numbers in [0, 1).
  */
@@ -19,7 +21,8 @@ public class LCG {
         this.a = a;
         this.c = c;
         this.k = k;
-        this.x = seed;
+        //this.x = seed;
+        reset(seed);
     }
 
     /**
@@ -28,8 +31,18 @@ public class LCG {
      * @return a uniform random number between 0 (inclusive) and 1 (exclusive)
      */
     public double nextUniform() {
-        // TODO
-        return 0.0;
+        // TODO implement  x_{n+1} = (a * x_n + c) mod k
+
+        BigInteger A = BigInteger.valueOf(a);
+        BigInteger C = BigInteger.valueOf(c);
+        BigInteger K = BigInteger.valueOf(k);
+        BigInteger X = new BigInteger(Long.toString(x));
+
+        BigInteger nextplus1 = A.multiply(X).add(C).mod(K);
+
+        nextplus1Output = nextplus1.longValue()
+
+        return (double) nextplus1Output / (double) k
     }
 
     /**
@@ -38,7 +51,10 @@ public class LCG {
      * @param seed the new seed value
      */
     public void reset(long seed) {
-        // TODO
-        return;
+        //make the reset to normalize to [0, k)
+
+        long r = seed % k;
+
+        this.x = (r < 0) ? r + k : r;
     }
 }
